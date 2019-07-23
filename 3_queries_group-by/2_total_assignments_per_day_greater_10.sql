fd8cd7054c7a8b4bd735f43fcc4dabfa972f91da
tylerccaceres <tyler.caceres@gmail.com>
2019-07-22 20:38:56 -0400

    inclusion of basic work minus the stretch work

--- 3_queries_group-by/2_total_assignments_per_day_greater_10.sql
+++ 3_queries_group-by/2_total_assignments_per_day_greater_10.sql
@@ -1,5 +1,12 @@
-SELECT day, count(*) AS total_assignments
-FROM assignments
-GROUP BY day
-HAVING count(*) >=10
-ORDER BY day;
+SELECT
+  day,
+  count(*) AS total_assignments
+FROM
+  assignments
+GROUP BY
+  day
+HAVING
+  count(*) >= 10
+ORDER BY
+  day;
+
--- 3_queries_group-by/3_large_cohorts_greater_18_students.sql
+++ 3_queries_group-by/3_large_cohorts_greater_18_students.sql
@@ -1,6 +1,13 @@
-SELECT c.name AS cohort_name, count(s.id) AS student_count
-FROM cohorts c
+SELECT
+  c.name AS cohort_name,
+  count(s.id) AS student_count
+FROM
+  cohorts c
   JOIN students s ON c.id = s.cohort_id
-GROUP BY c.name
-HAVING count(s.id) >= 18
-ORDER BY sum(s.id);
+GROUP BY
+  c.name
+HAVING
+  count(s.id) >= 18
+ORDER BY
+  sum(s.id);
+
